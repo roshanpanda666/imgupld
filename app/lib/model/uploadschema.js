898bdce7c6809c851676a888117b373cbee51a1f
roshan <roshanpanda666@gmail.com>
2025-08-28 02:17:45 +0530

    ...

--- app/lib/model/uploadschema.js
+++ app/lib/model/uploadschema.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const ImageSchema = new mongoose.Schema({
     img1: { type: String, required: true }, // Base64 string
-    fileName: { type: String },             // optional: image name
+    fileName: { type: String },             // optional: image name..
     fileType: { type: String },             // optional: image type (jpeg/png)
     fileSize: { type: Number },             // optional: size in bytes
     uploadedAt: { type: Date, default: Date.now },
